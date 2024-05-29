19eba7affb1806aabe7ece53af03873f17686a8d
Chefboyadee <98187420+Chefboyadee@users.noreply.github.com>
2024-05-29 17:09:10 -0400

    Navbar edits (absolute button fixes), color scheme, drivers page done

--- src/DriverApi.js
+++ src/DriverApi.js
@@ -41,9 +41,9 @@ export default function DriverApi(){
     }, []);
 
     return (
-        <div className="container mx-auto px-4">
-            <h1 className="text-3xl font-bold mb-6">Drivers on the {currentYear} grid!</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="container mx-auto px-4 mt-24 mb-16">
+            <h1 className="text-3xl text-red-700 text-center font-mono mb-12">Get to know the superhumans in this {currentYear} season driving on the grid!</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
                 {drivers.map(driver => (
                     <a
                         key={driver.driver_number}
@@ -52,19 +52,21 @@ export default function DriverApi(){
                         rel="noopener noreferrer"
                         className="no-underline"
                     >
-                        <div
-                            className="card-container rounded overflow-hidden shadow-lg border border-black"
-                            style={{ backgroundColor: `#${driver.team_colour}` }}
-                        >
-                            <div className="max-w-xs">
-                                <img className="w-full" src={driver.headshot_url} alt={`${driver.full_name} headshot`} />
-                                <div className="px-6 py-4">
-                                    <div className="font-bold text-xl mb-2">{driver.full_name} ({driver.name_acronym})</div>
-                                    <p className="text-gray-100 text-base">Driver Number: {driver.driver_number}</p>
-                                    <p className="text-gray-100 text-base">Country: {driver.country_code}</p>
-                                </div>
-                                <div className="px-6 pt-4 pb-2">
-                                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{driver.team_name}</span>
+                        <div className='hover:scale-105 shadow-2xl'>
+                            <div
+                                className="card-container rounded-3xl overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 hover:animate-shake"
+                                style={{ backgroundColor: `#${driver.team_colour}` }}
+                            >
+                                <div className="max-w-xs">
+                                    <img className="w-full mr-4" src={driver.headshot_url} alt={`${driver.full_name} headshot`} />
+                                    <div className="px-6 py-4">
+                                        <div className="font-bold font-mono text-lg mb-2">{driver.full_name} ({driver.name_acronym})</div>
+                                        <p className="text-gray-950	 font-mono text-lg font-bold">Driver Number: {driver.driver_number}</p>
+                                        <p className="text-fuchsia-900 font-mono font-semibold">Country: {driver.country_code}</p>
+                                    </div>
+                                    <div className="px-6 pt-4 pb-2">
+                                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{driver.team_name}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import f1logo from './photos/F1 Formula 1.svg';
 
-function Navbar() {
+export default function Navbar() {
+
+    const location = useLocation();
+    const isNotRootPath = location.pathname !== '/';
+
+
   return (
-    <nav className="bg-gray-800 p-4 fixed w-full top-0 left-0 flex items-center">
+    <nav
+      className={`bg-blue-600 p-4 mb-24 absolute w-full top-0 left-0 flex items-center ${
+        isNotRootPath ? 'z-50' : ''
+      }`}
+    >
       <div className="flex items-center space-x-4">
         <Link to="/">
           <img src={f1logo} alt="Logo" className="w-10 h-10"/>
         </Link>
         <Link to="/drivers">
-          <button className="bg-gray-500 border-2 border-black text-black font-bold py-2 px-4 rounded-full">
+          <button className="bg-purple-500 border-2 border-black text-black font-mono font-bold py-2 px-4 rounded-full">
             Drivers
           </button>
         </Link>
         <Link to="/radio">
-          <button className="bg-gray-500 border-2 border-black text-black font-bold py-2 px-4 rounded-full">
+          <button className="bg-purple-500 border-2 border-black text-black font-mono font-bold py-2 px-4 rounded-full">
             Radio
           </button>
         </Link>
@@ -23,4 +32,3 @@ function Navbar() {
   );
 }
 
-export default Navbar;
